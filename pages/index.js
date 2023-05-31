f15bb2dc7e5c8bf6e4fc8d5905d20de1a8854e3d
lil-j <jake@krunchy.net>
2023-05-31 19:19:12 +0200

    init

--- pages/index.js
+++ pages/index.js
@@ -72,11 +72,11 @@ export default function Home({gpas}) {
                         {/*    className="h-6"*/}
                         {/*    src="chrome_store.png"/>*/}
                         {/*Play button svg*/}
-                        <svg className="h-6 w-6" fill="none" stroke="white" viewBox="0 0 24 24"
-                                xmlns="http://www.w3.org/2000/svg">
-                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
-                                    d="M5 15l7-7 7 7"/>
-                        </svg>
+                        {/*<svg className="h-6 w-6" fill="none" stroke="white" viewBox="0 0 24 24"*/}
+                        {/*        xmlns="http://www.w3.org/2000/svg">*/}
+                        {/*    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"*/}
+                        {/*            d="M5 15l7-7 7 7"/>*/}
+                        {/*</svg>*/}
                         <p>See a demo</p>
                     </button>
                 </div>
